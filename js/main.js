8ba12e25fed1c3763411a122465e04fd164df825
Josh Thomas <joshuathomas2@mail.weber.edu>
2020-01-11 00:57:38 -0700

    Added missing lets

--- js/main.js
+++ js/main.js
@@ -60,23 +60,23 @@ function run_game(player_selection)
 
 function add_result(result, computer_selection, player_selection)
 {
-    result_key =
+    let result_key =
     {
         0: "LOSE",
         1: "WIN",
         2: "DRAW"
     };
 
-    color_key = 
+    let color_key = 
     {
         0: "bg-danger",
         1: "bg-primary",
         2: "bg-warning"
     };
 
-    results = document.getElementById("results");
+    let results = document.getElementById("results");
 
-    result_text = `<p class='${color_key[result]} m-0'>${result_key[result]} The player chose ${player_selection} and the computer chose ${computer_selection}</p>`;
+    let result_text = `<p class='${color_key[result]} m-0'>${result_key[result]} The player chose ${player_selection} and the computer chose ${computer_selection}</p>`;
 
     results.innerHTML = result_text + results.innerHTML;
 }
